9282b0c6b234e192f4161010fedc5b9a029b1b73
Flip Sasser <flip@x451.com>
2009-11-19 23:50:05 -0500

    Tweaks on the homepage

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,4 +1,5 @@
 $(function() {
+  var items = $('#carousel .item');
   var links = $('#submenu a'), timeout, timeoutIndex = 0, timeoutLength = 5000;
   links.click(function(event) {
     if (timeout) {
@@ -7,12 +8,23 @@ $(function() {
     if (event) {
 		  event.preventDefault();
 	  }
+	  links.removeClass('active');
+	  var link = $(this);
 	  var active = $('#carousel .item.active');
+	  var next = active.next();
+	  if (next.length == 0) {
+	    next = items[0];
+	  }
 	  if (!active.hasClass(this.getAttribute('rel'))) {
 	    active.addClass('fading').fadeOut(1000, function() {
 	      active.css('display', '').removeClass('active fading');
 	    });
+	    next.addClass('animating');
+      // var h2 = next.find('h2');
+      // h2.css('top', -100);
+      // next.find('h2').animate({top: 0}, 1000);
 	    $('#carousel .item.' + this.getAttribute('rel')).addClass('active').css('display', '');
+	    link.addClass('active');
 	  }
   });
   var changeLinks = function() {

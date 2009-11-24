7c3b5bbe534c39c21f09bc0dd49c2a586c4988ce
Flip Sasser <flip@x451.com>
2009-11-23 21:50:39 -0500

    Added hover and better homepage animations

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,6 +1,6 @@
 $(function() {
   var items = $('#carousel .item');
-  var links = $('#submenu a'), timeout, timeoutIndex = 0, timeoutLength = 5000;
+  var links = $('#submenu a'), timeout, timeoutIndex = 0, timeoutLength = 2000;
   links.click(function(event) {
     if (timeout) {
       clearTimeout(timeout);
@@ -11,19 +11,20 @@ $(function() {
 	  links.removeClass('active');
 	  var link = $(this);
 	  var active = $('#carousel .item.active');
-	  var next = active.next();
-	  if (next.length == 0) {
-	    next = items[0];
-	  }
-	  if (!active.hasClass(this.getAttribute('rel'))) {
-	    active.addClass('fading').fadeOut(1000, function() {
+    var next = items.filter('.' + link.attr('rel'));
+	  if (!active.hasClass(link.attr('rel'))) {
+	    active.addClass('fading').fadeOut(500, function() {
 	      active.css('display', '').removeClass('active fading');
 	    });
-	    next.addClass('animating');
-      // var h2 = next.find('h2');
-      // h2.css('top', -100);
-      // next.find('h2').animate({top: 0}, 1000);
-	    $('#carousel .item.' + this.getAttribute('rel')).addClass('active').css('display', '');
+      next.addClass('animating active').css('display', '');
+      var h2 = next.find('h2');
+      h2.css('top', -100).animate({top: 0}, 1000);
+      var p = next.find('p:first');
+      p.css('left', -300).animate({left: 0}, 1000);
+      var learnMore = next.find('p:last');
+      learnMore.css('top', 100).animate({top: 0}, 1000, function() {
+        next.removeClass('animating');
+      });
 	    link.addClass('active');
 	  }
   });
